cc6cbe78d2f168728d196003326f740ecae1f425
Maximuss1 <maxim.July16@gmail.com>
2021-11-03 19:10:48 +0200

    корзина не удалась!!!!

--- app/src/main/java/com/example/salesapp/CartShopPage.java
+++ app/src/main/java/com/example/salesapp/CartShopPage.java
@@ -24,7 +24,20 @@ public class CartShopPage extends AppCompatActivity {
     TextView main_scene, about_us;
     RecyclerView itemRecycler;
     static CartItemAdapter cartItemAdapter;
-    static List<CartItem> cartItemList = new ArrayList<>();
+
+    static List<CartItem> tavrList = new ArrayList<>();
+    static List<CartItem> citrList = new ArrayList<>();
+    static List<CartItem> metrList = new ArrayList<>();
+    static List<CartItem> wiakList = new ArrayList<>();
+    static List<CartItem> sinList = new ArrayList<>();
+    static List<CartItem> alloList = new ArrayList<>();
+    static List<CartItem> staffList = new ArrayList<>();
+    static List<CartItem> foxList = new ArrayList<>();
+    static List<CartItem> atbList = new ArrayList<>();
+    static List<CartItem> urbList = new ArrayList<>();
+    static List<CartItem> acList = new ArrayList<>();
+    static List<CartItem> ciberList = new ArrayList<>();
+    static List<CartItem> sprList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,103 +56,133 @@ public class CartShopPage extends AppCompatActivity {
         about_us = findViewById(R.id.about_us);
         about_us.setOnClickListener(v -> openAboutActivity());
 
-        cartItemList.clear();
+        tavrList.clear();
+        citrList.clear();
+        metrList.clear();
+
+        wiakList.clear();
+        sinList.clear();
+        alloList.clear();
+
+        staffList.clear();
+        foxList.clear();
+        atbList.clear();
+
+        urbList.clear();
+        acList.clear();
+        ciberList.clear();
+        sprList.clear();
 
         if (title.getText().equals("Tavriya")){
             for (Item item : ShopPage.fullItemList) {
                 if (Order.tavr_id.contains(item.getId())){
 
-                    cartItemList.add(new CartItem(item.getId(), item.getTitle(), item.getPrice2()));
+                    tavrList.add(new CartItem(item.getId(), item.getTitle(), item.getPrice2()));
+
                 }
             }
+
+            setItemRecycler(tavrList);
         }
         if (title.getText().equals("Citrus")){
             for (Item item : ShopPage.fullItemList) {
                 if (Order.citr_id.contains(item.getId())){
-                    cartItemList.add(new CartItem(item.getId(), item.getTitle(), item.getPrice2()));
+                    citrList.add(new CartItem(item.getId(), item.getTitle(), item.getPrice2()));
                 }
             }
+            setItemRecycler(citrList);
         }
         if (title.getText().equals("Metro")){
             for (Item item : ShopPage.fullItemList) {
                 if (Order.metr_id.contains(item.getId())){
-                    cartItemList.add(new CartItem(item.getId(), item.getTitle(), item.getPrice2()));
+                    metrList.add(new CartItem(item.getId(), item.getTitle(), item.getPrice2()));
                 }
             }
+            setItemRecycler(metrList);
         }
         if (title.getText().equals("LC Waikiki")){
             for (Item item : ShopPage.fullItemList) {
                 if (Order.waikiki_id.contains(item.getId())){
-                    cartItemList.add(new CartItem(item.getId(), item.getTitle(), item.getPrice2()));
+                    wiakList.add(new CartItem(item.getId(), item.getTitle(), item.getPrice2()));
                 }
             }
+            setItemRecycler(wiakList);
         }
         if (title.getText().equals("Sinsey")){
             for (Item item : ShopPage.fullItemList) {
                 if (Order.sin_id.contains(item.getId())){
-                    cartItemList.add(new CartItem(item.getId(), item.getTitle(), item.getPrice2()));
+                    sinList.add(new CartItem(item.getId(), item.getTitle(), item.getPrice2()));
                 }
             }
+            setItemRecycler(sinList);
         }
         if (title.getText().equals("Allo")){
             for (Item item : ShopPage.fullItemList) {
                 if (Order.allo_id.contains(item.getId())){
-                    cartItemList.add(new CartItem(item.getId(), item.getTitle(), item.getPrice2()));
+                    alloList.add(new CartItem(item.getId(), item.getTitle(), item.getPrice2()));
                 }
             }
+            setItemRecycler(alloList);
         }
         if (title.getText().equals("Staff")){
             for (Item item : ShopPage.fullItemList) {
                 if (Order.staff_id.contains(item.getId())){
-                    cartItemList.add(new CartItem(item.getId(), item.getTitle(), item.getPrice2()));
+                    staffList.add(new CartItem(item.getId(), item.getTitle(), item.getPrice2()));
                 }
             }
+            setItemRecycler(staffList);
         }
         if (title.getText().equals("Foxtrot")){
             for (Item item : ShopPage.fullItemList) {
                 if (Order.fox_id.contains(item.getId())){
-                    cartItemList.add(new CartItem(item.getId(), item.getTitle(), item.getPrice2()));
+                    foxList.add(new CartItem(item.getId(), item.getTitle(), item.getPrice2()));
                 }
             }
+            setItemRecycler(foxList);
         }
         if (title.getText().equals("ATB")){
             for (Item item : ShopPage.fullItemList) {
                 if (Order.atb_id.contains(item.getId())){
-                    cartItemList.add(new CartItem(item.getId(), item.getTitle(), item.getPrice2()));
+                    atbList.add(new CartItem(item.getId(), item.getTitle(), item.getPrice2()));
                 }
             }
+            setItemRecycler(atbList);
         }
         if (title.getText().equals("Urban Planet")){
             for (Item item : ShopPage.fullItemList) {
                 if (Order.urb_id.contains(item.getId())){
-                    cartItemList.add(new CartItem(item.getId(), item.getTitle(), item.getPrice2()));
+                    urbList.add(new CartItem(item.getId(), item.getTitle(), item.getPrice2()));
                 }
             }
+            setItemRecycler(urbList);
         }
         if (title.getText().equals("Aviatsiya")){
             for (Item item : ShopPage.fullItemList) {
                 if (Order.ac_id.contains(item.getId())){
-                    cartItemList.add(new CartItem(item.getId(), item.getTitle(), item.getPrice2()));
+                    acList.add(new CartItem(item.getId(), item.getTitle(), item.getPrice2()));
                 }
             }
+            setItemRecycler(acList);
         }
         if (title.getText().equals("Kibernetiki")){
             for (Item item : ShopPage.fullItemList) {
                 if (Order.ciber_id.contains(item.getId())){
-                    cartItemList.add(new CartItem(item.getId(), item.getTitle(), item.getPrice2()));
+                    ciberList.add(new CartItem(item.getId(), item.getTitle(), item.getPrice2()));
                 }
             }
+            setItemRecycler(ciberList);
         }
         if (title.getText().equals("Sportmaster")){
             for (Item item : ShopPage.fullItemList) {
                 if (Order.spr_id.contains(item.getId())){
-                    cartItemList.add(new CartItem(item.getId(), item.getTitle(), item.getPrice2()));
+                    sprList.add(new CartItem(item.getId(), item.getTitle(), item.getPrice2()));
                 }
             }
+            setItemRecycler(sprList);
         }
         //cartItemList.add(new CartItem(1, "d", "f"));
 
-        setItemRecycler(cartItemList);
+
     }
 
     private void setItemRecycler(List<CartItem> cartItemList) {
