c6fd29b0b86626ff4db55a614164f8b51fb00248
Smug <sashasmug@gmail.com>
2021-11-17 10:07:15 +0200

    пофиксил лс валкили

--- app/src/main/java/com/example/salesapp/ShopPage.java
+++ app/src/main/java/com/example/salesapp/ShopPage.java
@@ -307,17 +307,17 @@ public class ShopPage extends AppCompatActivity {
                         }
                     }
                 }
-                for (int i = 0; i < 35; i++) {
+                for (int i = 0; i < 30; i++) {
 
                     itemList.add(new Item(25, name.get(i), old_price.get(i), new_price.get(i), photo.get(i), links.get(i)));
                 }
-//                itemList.add(new Item(25, name.get(0), old_price.get(0), new_price.get(0), photo.get(0), links.get(0)));
 //                itemList.add(new Item(26, name.get(1), old_price.get(1), new_price.get(1), photo.get(1), links.get(1)));
 //                itemList.add(new Item(27, name.get(2), old_price.get(2), new_price.get(2), photo.get(2), links.get(2)));
 //                itemList.add(new Item(28, name.get(3), old_price.get(3), new_price.get(3), photo.get(3), links.get(3)));
 //                itemList.add(new Item(29, name.get(4), old_price.get(4), new_price.get(4), photo.get(4), links.get(4)));
 //                itemList.add(new Item(30, name.get(5), old_price.get(5), new_price.get(5), photo.get(5), links.get(5)));
 //                itemList.add(new Item(31, name.get(6), old_price.get(6), new_price.get(6), photo.get(6), links.get(6)));
+//                itemList.add(new Item(25, name.get(0), old_price.get(0), new_price.get(0), photo.get(0), links.get(0)));
 //                itemList.add(new Item(32, name.get(7), old_price.get(7), new_price.get(7), photo.get(7), links.get(7)));
 
                 setItemRecycler(itemList);
