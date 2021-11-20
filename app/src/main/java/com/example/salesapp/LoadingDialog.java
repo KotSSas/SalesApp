d5ac0316ba17e0bbeacb599c2aeaa0882caeba44
Smug <sashasmug@gmail.com>
2021-11-20 15:16:31 +0200

    Раскоментил строку с выводом

--- app/src/main/java/com/example/salesapp/LoadingDialog.java
+++ app/src/main/java/com/example/salesapp/LoadingDialog.java
@@ -15,7 +15,7 @@ public class LoadingDialog {
     public  void startLoadingDialog(){
         AlertDialog.Builder builder = new AlertDialog.Builder(activity);
         LayoutInflater inflater = activity.getLayoutInflater();
-       // builder.setView(inflater.inflate(R.layout.custom_dialog, null));
+       builder.setView(inflater.inflate(R.layout.custom_dialog, null));
         builder.setCancelable(true);
         dialog = builder.create();
 
